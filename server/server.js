e6ad8054bf60c770b8fc4c7c5e9638259c52619b
hdyaro77 <harshit.dubey1923@gmail.com>
2022-06-14 12:17:12 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -6,13 +6,14 @@ const path = require("path");
 dotenv.config({ path: "./config.env" });
 app.use(cors());
 const port = process.env.PORT || 8080;
+__dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   // Handle React routing, return all requests to React app
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
   });
 }
 console.log(path.join(__dirname, "client/build", "index.html"))
